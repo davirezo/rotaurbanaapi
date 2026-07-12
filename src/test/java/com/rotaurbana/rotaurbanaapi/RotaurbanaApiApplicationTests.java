d5e051d0b5657b1d84195073b573000a34b7b092
davirezo <fksdavi@gmail.com>
2026-07-12 19:19:15 -0300

    Configurando projeto

---
+++ src/test/java/com/rotaurbana/rotaurbanaapi/RotaurbanaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotaurbana.rotaurbanaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaurbanaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
